aa7b4e9da7a3c680ca066f9244d881f6baaeddc6
HarikaReddy <228r1a62c6@gmail.com>
2025-12-17 15:45:21 +0530

    Create  __init__.py

---
+++ engine/__init__.py
@@ -0,0 +1,7 @@
+"""
+Engine package for WebGuardX AppSec Automation Framework.
+
+This package contains the core logic responsible for
+orchestrating vulnerability validation modules and
+coordinating report generation.
+"""
